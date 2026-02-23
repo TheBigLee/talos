92fd6028b6db77c7a23a3deb816deeae72746b5f
Nicolas Bigler <nicolas.bigler@vshn.ch>
2026-02-23 18:06:10 +0100

    Some talos admin stuff
    Signed-off-by: Nicolas Bigler <nicolas.bigler@vshn.ch>

--- tofu/encryption.tf
+++ tofu/encryption.tf
@@ -6,34 +6,42 @@ variable "passphrase" {
 
 terraform {
   encryption {
+    #    method "unencrypted" "migrate" {}
+
     ## Step 1: Add the desired key provider:
-    key_provider "pbkdf2" "my_key_provider_name" {
+    key_provider "pbkdf2" "pbkdf2_provider" {
       passphrase = var.passphrase
     }
     ## Step 2: Set up your encryption method:
-    method "aes_gcm" "my_method_name" {
-      keys = key_provider.pbkdf2.my_key_provider_name
+    method "aes_gcm" "encrypt_tf_state" {
+      keys = key_provider.pbkdf2.pbkdf2_provider
     }
 
     state {
       ## Step 3: Link the desired encryption method:
-      method = method.aes_gcm.my_method_name
+      method = method.aes_gcm.encrypt_tf_state
 
       ## Step 4: Run "tofu apply".
 
       ## Step 5: Consider adding the "enforced" option:
       enforced = true
+      #fallback {
+      #  method = method.unencrypted.migrate
+      #}
     }
 
     ## Step 6: Repeat steps 3-5 for plan{} if needed.
     plan {
       ## Step 3: Link the desired encryption method:
-      method = method.aes_gcm.my_method_name
+      method = method.aes_gcm.encrypt_tf_state
 
       ## Step 4: Run "tofu apply".
 
       ## Step 5: Consider adding the "enforced" option:
       enforced = true
+      #fallback {
+      #  method = method.unencrypted.migrate
+      #}
     }
   }
 }
